7d4ac8601eb5e2cb636e3984c8b3a63e7463cbae
Derek George <derek68038@gmail.com>
2015-04-09 13:53:32 -0500

    Practice creating custom theme for a WordPress project.

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  </head>
+  
+  <body>
+    <div>
+      <p><a href="<?php bloginfo('url'); ?>">Home</a></p>
+    </div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+   <?php while (have_posts()) : the_post(); ?>
+         
+         
+         
+         <h2>
+           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> - <?php the_field("stars"); ?> Stars
+         </h2>
+         <?php the_content(); ?>
+   
+   
+   <?php
+   endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
