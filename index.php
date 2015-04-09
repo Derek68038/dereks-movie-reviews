cebd8d7b97d289c7054720ed0b8706257f6b59ca
Derek George <derek68038@gmail.com>
2015-04-09 14:01:08 -0500

    Edited spacing.

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : ?>
-   <?php while (have_posts()) : the_post(); ?>
-         
-         
-         
-         <h2>
-           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> - <?php the_field("stars"); ?> Stars
-         </h2>
-         <?php the_content(); ?>
-   
-   
+   <?php while (have_posts()) : the_post(); ?>    
+       <h2>
+         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> - <?php the_field("stars"); ?> Stars
+       </h2>
+       <?php the_content(); ?>
    <?php
    endwhile;
 endif;
